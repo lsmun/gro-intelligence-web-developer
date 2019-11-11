732562c8651cd12dfbb356852910dc8d319e708e
Laura Mun <laurasumun@gmail.com>
2019-11-11 17:50:54 -0500

    Add remaining scaffolding. UI complete.

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,3 @@
-/*function toggleDropdown (e) {
-  const _d = $(e.target).closest('.dropdown'),
-    _m = $('.dropdown-menu', _d);
-  setTimeout(function(){
-    const shouldOpen = e.type !== 'click' && _d.is(':hover');
-    _m.toggleClass('show', shouldOpen);
-    _d.toggleClass('show', shouldOpen);
-    $('[data-toggle="dropdown"]', _d).attr('aria-expanded', shouldOpen);
-  }, e.type === 'mouseleave' ? 300 : 0);
-}
-
-$('body')
-  .on('mouseenter mouseleave','.dropdown',toggleDropdown)
-  .on('click', '.dropdown-menu a', toggleDropdown);*/
-
 $(document).ready(function() {
     'use strict';
     $(".navbar .nav .dropdown").hoverIntent({
@@ -24,6 +9,6 @@ $(document).ready(function() {
             $(this).find('a.nav-link').removeClass('arrow-up');
             $(this).find(".dropdown-menu").toggle();
         },
-        timeout: 50
+        timeout: 100
     });
 });
